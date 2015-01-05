cf1c1eee09363cb1d492d2b36a75d803db949d15
Ford <ford.peprah@uwaterloo.ca>
2015-01-05 15:03:50 -0500

    Fix link when migrated to PC

--- Checkouts/menu.php
+++ Checkouts/menu.php
@@ -1,6 +1,6 @@
 <div class="nav">
   <ul>
-    <li><a href="/">Checkouts</a></li>
+    <li><a href="index.php">Checkouts</a></li>
     <li><a href="candy.php">Candy</a></li>
     <li><a href="catalog.php">Catalog</a></li>
     <li><a href="people.php">People</a></li>
